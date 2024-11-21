ec338f201066fd6afa49f83859af47c0577c0af0
Varsha Hindupur <hindupur.v@northeastern.edu>
2024-11-21 13:28:11 -0500

    initial with basic setups added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Medicine-delivery-app-kubernetes-deployment
-This repository is created to deploy web app in kubernetes.
+I'm using this repository for deploying the medicine delivery app on Kubernetes.
+
+# commands
+docker build -t medicine-backend:1.0 .
+docker run -p 5001:5001 --env-file ./backend/.env medicine-backend:1.0
+
+docker build -t medicine-frontend:1.0 .
+docker run -p 3000:80 medicine-frontend:1.0
---
+++ backend/server.js
@@ -0,0 +1,39 @@
+//server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/Auth");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
+const cors = require("cors");
+const http = require("http");
+const dotenv = require("dotenv");
+dotenv.config();
+// ... (other code)
+
+
+dotenv.config();
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(console.log("Mongodb Connected"))
+.catch((err)=> console.log(err))
+
+const server = http.createServer(app); // Create an HTTP server
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/users' , userRoute);
+app.use('/api/products' , productRoute);
+app.use('/api/orders' , orderRoute);
+app.use('/api/cart' , cartRoute);
+
+
+server.listen(5001,function(){
+    console.log("Server is listening on port 5000");
+});
